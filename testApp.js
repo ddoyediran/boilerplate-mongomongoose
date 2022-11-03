20ba627dabb1e388d6b87476d71059d546d6d948
ddoyediran <ddoyediran@gmail.com>
2022-11-03 23:50:50 +0100

    more commit

--- testApp.js
+++ testApp.js
@@ -27,3 +27,32 @@ msg
 // }
 
 // saveToDatabase();
+
+// search query
+EmailModel.find({ email: "ad@m.com" })
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// update record
+EmailModel.findOneAndUpdate(
+  {
+    email: "ad@m.com",
+  },
+  {
+    email: "ad22@m.com",
+  },
+  {
+    new: true,
+    runValidators: true,
+  }
+)
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
